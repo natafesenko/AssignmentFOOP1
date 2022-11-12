9112417fc1a3b206ae6337bcbe0719ab5731792e
Natali Fesenko <22nataliiafesenko@mayocollege.ie>
2022-11-12 23:22:39 +0000

    checking code, intermediate result

--- CountYourBMI/src/CountBMI.java
+++ CountYourBMI/src/CountBMI.java
@@ -14,15 +14,10 @@ public class CountBMI {
 
 		
 		
+		
+	}	
 	
-
-		
-
-		
-		
-		
-	
-	public static void welcomeMessage(String[] args) {
+	public static void welcomeMessage() {
 		
 		// Welcome message
 				System.out.println(" ██████╗ █████╗ ██╗      ██████╗██╗   ██╗██╗      █████╗ ████████╗███████╗    ██╗   ██╗ ██████╗ ██╗   ██╗██████╗     ██████╗ ███╗   ███╗██╗");
@@ -34,86 +29,115 @@ public class CountBMI {
 		
 	}
 	
-	public static void UsersInput {
+	public static void UsersInputWeight(double weight) {
 		
 		// creates a new Scanner instance which points to the input stream passed as argument
-				Scanner in = new Scanner(System.in);
+		Scanner in = new Scanner(System.in);
+		
+        if (Double.doubleToRawLongBits(weight) <= 0) {
+            System.err.println("you must weight something!");
+          }
 				
-				// work with variable weight
-				while(true) {
-					try {
-						System.out.println("What your Weight? (lb)");
-						// initial variable weight
-						weight = in.nextDouble();
-						System.out.println("Thanks");
-						break;
-						// processing the error
-					}catch(InputMismatchException ex) {
-						System.out.println("Error in your input");
-						//resets this scanner
-						in.reset();
-						return;
-						}
-					
-					// work with variable height
-					while(true) {
-						try {
-							System.out.println("What your Height? (in)");
-							// initial variable height
-							height = in.nextDouble();
-							System.out.println("Thanks");
-							break;
-						// processing the error
-						}catch(InputMismatchException ex) {
-							System.out.println("Error in your input");
-							//resets this scanner
-							in.reset();
-						}
-					}
+		// work with variable weight
+		while(true) {
+			try {
+				System.out.println("What your Weight? (lb)");
+				// initial variable weight
+				weight = in.nextDouble();
+				System.out.println("Thanks");
+				break;
+				// processing the error
+			}catch(InputMismatchException ex) {
+				System.out.println("Error in your input");
+				//resets this scanner
+				in.reset();
+				return;
+			}
+				
+		}			
+	}
+	
+	public static void UsersInput(double height) {
+		
+		Scanner in = new Scanner(System.in);
+		
+       if (Double.doubleToRawLongBits(height) <= 0) {
+            System.err.println("you must have height!");
+          }
+		 //work with variable height
+		while(true) {
+			try {
+				System.out.println("What your Height? (in)");
+				// initial variable height
+				height = in.nextDouble();
+			System.out.println("Thanks");
+				break;
+			// processing the error
+			}catch(InputMismatchException ex) {
+				System.out.println("Error in your input");
+				//resets this scanner
+				in.reset();
+			}
+		}
+		
+	}
+	
+	public static void CountBmi(double bmi, double weight, double height) {
+		// Count BMI in Imperial BMI Formula
+		// BMI = weight (lb) * 703 / (height (in))2	
+		bmi = (weight/(height*height)*703);
 		
 	}
 				
-				
-				
-				
-				// Count BMI in Imperial BMI Formula
-				// BMI = weight (lb) * 703 / (height (in))2	
-				bmi = (weight/(height*height)*703);
-				
-				// Casting int to double
-				int intBmi = (int) bmi;
-				
-				System.out.println("Your BMI is " + intBmi);
-				
-				if (intBmi < 15 ) {
-					System.out.println("You have a Very severely underweight");
-				}
-				else if (intBmi <= 16) {
-					System.out.println("You have a Severely underweight");
-				}
-				else if (intBmi <= 18.5) {
-					System.out.println("You have a Underweight");
-				}
-				else if (intBmi <= 25) {
-					System.out.println("You have a Normal (healthy weight)");
-				}
-				else if (intBmi >= 30) {
-					System.out.println("You have a Overweight");
-				}
-				else if (intBmi >= 35) {
-					System.out.println("You have a Moderately obese");
-				}
-				else if (intBmi >= 40) {
-					System.out.println("You have a Severely obese");
-				}
-				else if (intBmi > 40 ) {
-					System.out.println("You have a Very severely obese");
-				}
-
-			}
-
+	public static void CastingBmi(double bmi) {
 		
-				
+		// Casting int to double
+		Scanner in = new Scanner(System.in);
+		int question = in.nextInt();
+		System.out.println("Round your BMI? If answer yes, you should write 1, if no - 0");
+		if (question == 1) { 
+			int intBmi = (int) bmi;
+			System.out.println("Your BMI is " + intBmi);
+		}
+		else if(question == 0) {
+			System.out.println("Your BMI is " + bmi);
+		}
+			
+		
+	}
+	
+	public static void compareBMI (double bmi, int intBmi) {
+		
+		System.out.println("Your BMI is " + intBmi);
+		
+		if (bmi < 15 ) {
+			System.out.println("You have a Very severely underweight");
+		}
+		else if (bmi <= 16) {
+			System.out.println("You have a Severely underweight");
+		}
+		else if (bmi <= 18.5) {
+			System.out.println("You have a Underweight");
+		}
+		else if (bmi <= 25) {
+			System.out.println("You have a Normal (healthy weight)");
+		}
+		else if (bmi >= 30) {
+			System.out.println("You have a Overweight");
+		}
+		else if (bmi >= 35) {
+			System.out.println("You have a Moderately obese");
+		}
+		else if (bmi >= 40) {
+			System.out.println("You have a Severely obese");
+		}
+		else if (bmi > 40 ) {
+			System.out.println("You have a Very severely obese");
+		}
+		
+		
+	}
+	
 				
 
 }
